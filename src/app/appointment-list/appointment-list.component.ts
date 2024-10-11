43c5beda5b9e122f3c4318eeb8030613d1793255
snigdha <sunny.rangineni@gmail.com>
2024-10-11 13:28:40 -0500

    One way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-  appointment: string = "Take dog for a walk";
+  appointment: Appointment = {
+    id: 1,
+    title: "Take dog for a walk",
+    date: new Date('2023-07-30')
+  }
 }
