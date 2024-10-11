b5270b32c12d83db0620b3673ddb570e637a6b27
snigdha <sunny.rangineni@gmail.com>
2024-10-11 15:36:00 -0500

    Adding appointments to Array

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -13,6 +13,19 @@ export class AppointmentListComponent {
   appointments: Appointment[] = [];
 
   addAppointment() {
-    alert(this.newAppointmentTitle + " " + this.newAppointmentDate)
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment : Appointment = {
+        id: Date.now(),
+        title:this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+
+      this.appointments.push(newAppointment);
+
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate = new Date();
+
+      alert(this.appointments.length)
+    }
   }
 }
